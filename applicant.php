54145c0cb7f37cdaceba28ff3a0f1e5cfd9cdf5b
Sahatthaya Thianmongkhon <sahatthaya.t@gmail.com>
2022-08-04 14:26:38 +0700

    no comment

--- applicant.php
+++ applicant.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cvbn</title>
+    <title>ส่งคำร้องขอเป็นพาร์ทเนอร์</title>
     <link rel="stylesheet" href="./css/applicant.css" type="text/css">
 </head>
 <script type="text/javascript">
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>หน้าหลัก55555555</title>
+    <title>หน้าหลัก</title>
     <!-- css  -->
     <link rel="stylesheet" href="./css/index.css" type="text/css">
 </head>
