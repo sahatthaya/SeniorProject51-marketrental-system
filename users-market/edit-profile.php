99e2409079a3d3a10ccf270790aa4b995cda4f2e
Sahatthaya Thianmongkhon <sahatthaya.t@gmail.com>
2022-09-06 15:25:13 +0700

    09/06/2022

--- users-market/edit-profile.php
+++ users-market/edit-profile.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/password.css" type="text/css">
-    <link rel="stylesheet" href="../css/applicant.css">
     <title> MarketRental - แก้ไขข้อมูลบัญชีผู้ใช้</title>
     
 </head>
