92a00d2b4cbe8967e43dedc2787f2922f48b3ef2
Sahatthaya Thianmongkhon <sahatthaya.t@gmail.com>
2022-09-07 15:18:01 +0700

    09/07/2022 responsive

--- marketPlan.php
+++ marketPlan.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no">
     <title> MarketRental - แก้ไขแผนผังตลาด</title>
     <!-- css  -->
-    <link rel="stylesheet" href="./css/market-plan.css" type="text/css">
+    <link rel="stylesheet" href="./css/marketplan.css" type="text/css">
 
     <?php
     include "profilebar.php";
--- users-merchant/complain.php
+++ users-merchant/complain.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> MarketRental - ระบบร้องเรียน</title>
     <link rel="stylesheet" href="../css/complain.css">
-    <link rel="stylesheet" href="../css/banner.css">
 
 </head>
 
