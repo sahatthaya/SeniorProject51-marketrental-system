362c8d497582991c2dfce21d2c8429be75fa10ae
pixeliology <72910615+sahatthaya@users.noreply.github.com>
2022-11-23 20:12:48 +0700

    23/11/2022 เพิ่ม pagination nologin

--- all-market.php
+++ all-market.php
@@ -15,66 +15,74 @@ include "./nav.php";
 include "./backend/1-connectDB.php";
 include "backend/1-import-link.php";
 require "./backend/qry-allmarket.php";
-
 ?>
 
 <body>
     <div class="wrap">
         <div class="top">
             <h1 id="headline">ตลาดทั้งหมด</h1>
-            <form name="Searchbox" method="POST" class="search_box">
+            <!-- <form name="Searchbox" method="POST" class="search_box">
                 <div class="search-input">
                     <label for="#search">
                         <h5>ค้นหา : </h5>
                     </label>
-                    <input class="btn border-secondary" type="text" name="search" id="search" placeholder="กรอกข้อมูลที่ต้องการค้นหา" onkeyup="search(this.value)">
+                    <input class="btn border-secondary" type="text" name="search" id="search" placeholder="กรอกข้อมูลที่ต้องการค้นหา">
                 </div>
                 <div class="search-btn">
                     <input class="btn btn-primary " id="search" type="submit" name="searchsubmit" value="ค้นหา">
                     <input class="btn btn-danger" id="reset" type="submit" name="reset" value="รีเซ็ต">
                 </div>
-            </form>
-            <!-- <div class=" topmkr">
-                <div class="typemkr">
-                    <a class="quick text-decoration-none" id="partner-btn" style="background-color: #317DD9;">
-                        <p class="text-center pt-3 "> ตลาดสด</p>
-                    </a>
-                    <a class="quick text-decoration-none" id="partner-btn" style="background-color: #005DB5;">
-                        <p class="text-center pt-3 "> ตลาดเปิดท้าย</p>
-                    </a>
-                    <a class="quick text-decoration-none" id="partner-btn" style="background-color: #004091;">
-                        <p class="text-center pt-3 "> ตลาดค้าส่ง</p>
-                    </a>
-                    <a class="quick text-decoration-none" id="partner-btn" style="background-color: #00256F;">
-                        <p class="text-center pt-3 "> อื่นๆ</p>
-                    </a>
-                </div>
-            </div> -->
-        </div>
-        <hr>
-        <div class="box">
-            <?php while ($row = $result->fetch_assoc()) : ?>
-                <a class="marketcard radius10" href="market-info.php?mkr_id=<?php echo $row['mkr_id']; ?>">
-                    <img src="./<?php echo $row['mkr_pic'] ?>" class="radius10 mkrimg " alt="...">
-                    <div class="overlay p-3">
-                        <h4 style="text-align: center;"><?php echo $row['mkr_name'] ?></h4>
-                        <div class="d-flex justify-content-between markettag px-3">
-                            <div class="ptext"><i class='bx bxs-navigation'></i> <?php echo $row['province_name'] ?></div>
-                            <div class="ptext"><i class='bx bxs-info-circle'></i> <?php echo $row['market_type'] ?></div>
-                        </div>
-                        <p class="ptext">เบอร์ติดต่อ : <?php echo $row['tel'] ?></p>
-                        <p class="ptext">อีเมล :
-                            <?php echo $row['email']; ?>
-                        </p>
-                        <p class="ptext">วันทำการ :
-                            <?php echo $row['opening']; ?>
-                        </p>
-                    </div>
-                </a>
-            <?php
-            endwhile ?>
+            </form> -->
         </div>
     </div>
+    <hr class="mt-0">
+    <div class="box">
+        <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+            <a class="marketcard radius10" href="market-info.php?mkr_id=<?php echo $row['mkr_id']; ?>">
+                <img src="./<?php echo $row['mkr_pic'] ?>" class="radius10 mkrimg " alt="...">
+                <div class="overlay p-3">
+                    <h4 style="text-align: center;"><?php echo $row['mkr_name'] ?></h4>
+                    <div class="d-flex justify-content-between markettag px-3">
+                        <div class="ptext"><i class='bx bxs-navigation'></i> <?php echo $row['province_name'] ?></div>
+                        <div class="ptext"><i class='bx bxs-info-circle'></i> <?php echo $row['market_type'] ?></div>
+                    </div>
+                    <p class="ptext">เบอร์ติดต่อ : <?php echo $row['tel'] ?></p>
+                    <p class="ptext">อีเมล :
+                        <?php echo $row['email']; ?>
+                    </p>
+                    <p class="ptext">วันทำการ :
+                        <?php echo $row['opening']; ?>
+                    </p>
+                </div>
+            </a>
+        <?php
+        endwhile ?>
+    </div>
+    </div>
+    <hr>
+    <?php
+    $sql2 = "SELECT * FROM market_detail WHERE (a_id='1')";
+    $query2 = mysqli_query($conn, $sql2);
+    $total_record = mysqli_num_rows($query2);
+    $total_page = ceil($total_record / $perpage);
+    ?>
+    <nav aria-label="Page navigation example" style="height: 38px;">
+        <ul class="pagination justify-content-end">
+            <li class="page-item">
+                <a class="page-link" href="all-market.php?page=1" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+            <?php for ($i = 1; $i <= $total_page; $i++) { ?>
+                <li class="page-item"><a class="page-link" href="all-market.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></a></li>
+            <?php } ?>
+            <li class="page-item">
+                <a class="page-link" href="all-market.php?page=<?php echo $total_page; ?>" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
     <!-- <script type="text/javascript">
         $(document).ready(function() {
             $("#search").keyup(function() {
--- backend/qry-allmarket.php
+++ backend/qry-allmarket.php
@@ -1,4 +1,17 @@
 <?php
+$perpage = 12;
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 1;
+}
+$start = ($page - 1) * $perpage;
+
+$query_province = "SELECT * FROM provinces";
+$result_province = mysqli_query($conn, $query_province);
+$query_mkrType = "SELECT * FROM market_type ORDER BY market_type_id";
+$result_mkrType = mysqli_query($conn, $query_mkrType);
+
 // mkr query
 $data = "SELECT market_detail.*,users.username ,
 provinces.province_name,
@@ -11,57 +24,40 @@ FROM market_detail
     JOIN amphures ON (market_detail.	amphure_id = amphures.id)
     JOIN districts ON (market_detail.district_id = districts.id)
     JOIN market_type ON (market_detail.market_type_id = market_type.market_type_id)
-ORDER BY mkr_id DESC
-WHERE (a_id='1')";
+WHERE (a_id='1') ORDER BY mkr_id DESC limit {$start} , {$perpage} ";
 $result = mysqli_query($conn, $data);
 
-if (isset($_POST["searchsubmit"])) {
-    $searchkey = $_POST['search'];
-    $data = "SELECT market_detail.*,users.username ,
-    provinces.province_name,
-    amphures.amphure_name,
-    districts.district_name , 
-    market_type.market_type
-    FROM market_detail 
-        JOIN users ON (market_detail.users_id = users.users_id)
-        JOIN provinces ON (market_detail.province_id = provinces.id)
-        JOIN amphures ON (market_detail.	amphure_id = amphures.id)
-        JOIN districts ON (market_detail.district_id = districts.id)
-        JOIN market_type ON (market_detail.market_type_id = market_type.market_type_id)
-    WHERE (a_id='1' AND mkr_name LIKE '%" . $searchkey . "%' or mkr_name LIKE '%" . $searchkey . "%' or province_name LIKE '%" . $searchkey . "%' or market_type LIKE '%" . $searchkey . "%')";
-    $result = mysqli_query($conn, $data);
-} else {
-    $data = "SELECT market_detail.*,users.username ,
-    provinces.province_name,
-    amphures.amphure_name,
-    districts.district_name , 
-    market_type.market_type
-    FROM market_detail 
-        JOIN users ON (market_detail.users_id = users.users_id)
-        JOIN provinces ON (market_detail.province_id = provinces.id)
-        JOIN amphures ON (market_detail.	amphure_id = amphures.id)
-        JOIN districts ON (market_detail.district_id = districts.id)
-        JOIN market_type ON (market_detail.market_type_id = market_type.market_type_id)
-        WHERE (a_id='1')
-    ORDER BY mkr_id DESC ";
-    $result = mysqli_query($conn, $data);
-}
-if (isset($_POST["reset"])) {
-    $data = "SELECT market_detail.*,users.username ,
-    provinces.province_name,
-    amphures.amphure_name,
-    districts.district_name , 
-    market_type.market_type
-    FROM market_detail 
-        JOIN users ON (market_detail.users_id = users.users_id)
-        JOIN provinces ON (market_detail.province_id = provinces.id)
-        JOIN amphures ON (market_detail.	amphure_id = amphures.id)
-        JOIN districts ON (market_detail.district_id = districts.id)
-        JOIN market_type ON (market_detail.market_type_id = market_type.market_type_id)
-        WHERE (a_id='1')
-    ORDER BY mkr_id DESC ";
-    $result = mysqli_query($conn, $data);
-}
+// if (isset($_POST["searchsubmit"])) {
+//     $searchkey = $_POST['search'];
+//     $data = "SELECT market_detail.*,users.username ,
+//     provinces.province_name,
+//     amphures.amphure_name,
+//     districts.district_name , 
+//     market_type.market_type
+//     FROM market_detail 
+//         JOIN users ON (market_detail.users_id = users.users_id)
+//         JOIN provinces ON (market_detail.province_id = provinces.id)
+//         JOIN amphures ON (market_detail.	amphure_id = amphures.id)
+//         JOIN districts ON (market_detail.district_id = districts.id)
+//         JOIN market_type ON (market_detail.market_type_id = market_type.market_type_id)
+//     WHERE (a_id='1' AND mkr_name LIKE '%" . $searchkey . "%' or mkr_name LIKE '%" . $searchkey . "%' or province_name LIKE '%" . $searchkey . "%' or market_type LIKE '%" . $searchkey . "%')";
+//     $result = mysqli_query($conn, $data);
+// }
+// if (isset($_POST["reset"])) {
+//     $data = "SELECT market_detail.*,users.username ,
+//     provinces.province_name,
+//     amphures.amphure_name,
+//     districts.district_name , 
+//     market_type.market_type
+//     FROM market_detail 
+//         JOIN users ON (market_detail.users_id = users.users_id)
+//         JOIN provinces ON (market_detail.province_id = provinces.id)
+//         JOIN amphures ON (market_detail.	amphure_id = amphures.id)
+//         JOIN districts ON (market_detail.district_id = districts.id)
+//         JOIN market_type ON (market_detail.market_type_id = market_type.market_type_id)
+//     WHERE (a_id='1') ORDER BY mkr_id DESC limit {$start} , {$perpage} ";
+//     $result = mysqli_query($conn, $data);
+// }
 // top mkr query
 $topquerymkr = "SELECT market_detail.*,users.username ,
 provinces.province_name,
@@ -77,49 +73,3 @@ FROM market_detail
     WHERE (a_id='1')
 ORDER BY mkr_id DESC LIMIT 4 ";
 $topresultmkr = mysqli_query($conn, $topquerymkr);
-mysqli_close($conn);
-// $output ='';
-
-// if(isset($_POST['query'])){
-//     $search = $_POST['query'];
-//     $stmt =$conn->prepare("SELECT market_detail.*,province.province_name , market_type.market_type FROM market_detail 
-//     JOIN province ON (market_detail.province_id = province.province_id)
-//     JOIN market_type ON (market_detail.market_type_id = market_type.market_type_id)
-//     WHERE (mkr_name LIKE CONCAT('%',?,'%') or mkr_name LIKE CONCAT('%',?,'%') or province_name LIKE CONCAT('%',?,'%') or market_type LIKE CONCAT('%',?,'%')");
-//     $stmt->bind_param("ss",$search,$search);
-// }else{
-//     $stmt=$conn->prepare("SELECT market_detail.*,province.province_name , market_type.market_type FROM market_detail 
-//     JOIN province ON (market_detail.province_id = province.province_id)
-//     JOIN market_type ON (market_detail.market_type_id = market_type.market_type_id)
-//     ORDER BY mkr_id DESC");
-// }
-
-// $stmt->execute();
-// $result=$stmt->get_result();
-
-// if($result->num_rows>0){
-//     $output = " <div class='box'>";
-//     while ($row = $result->fetch_assoc()){
-//         $output.='
-//          <a id="market-item" class="marketcard radius10" href="all-mkr-mkrInfo.php?mkr_id='.$row['mkr_id'].'">
-//                     <img src="'.$row['mkr_pic'].'" class="radius10 mkrimg " alt="...">
-//                     <div class="overlay">
-//                         <h4 style="text-align: center;">'. $row['mkr_name'].'</h4>
-//                         <div class="row markettag">
-//                             <p class="col-6  ptext" style="text-align: center;"><i class="bx bxs-navigation"></i>'.$row['province_name'].'</p>
-//                             <p class="col-6  ptext" style="text-align: center;"><i class="bx bxs-info-circle"></i>'. $row['market_type'].'</p>
-
-//                         </div>
-//                         <p class="ptext">รายละเอียด : '.$row['mkr_descrip'].'</p>
-//                         <p class="ptext">เบอร์ติดต่อ : '.$row['tel'].'</p>
-//                         <p class="ptext">อีเมล : '.$row['email'].'</p>
-//                         <p class="ptext">จำนวนแผงว่าง : 0 จาก 0</p>
-//                     </div>
-//                 </a>
-//         ';
-//     }
-//     echo $output;
-
-// }else{
-//     echo "<h3> ไม่พบข้อมูล </h3>";
-// }
