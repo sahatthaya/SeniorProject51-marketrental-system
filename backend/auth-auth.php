0b3073d4f7eb46e1bbd716273db888ca1e9e3cf9
pixeliology <72910615+sahatthaya@users.noreply.github.com>
2023-03-19 14:09:10 +0700

    19/03/2023 fix auth

--- backend/auth-auth.php
+++ backend/auth-auth.php
@@ -1,65 +1,58 @@
-<?session_start();?>
+<? session_start(); ?>
 
 <?php
 
-    include "backend/1-connectDB.php";
+include "backend/1-connectDB.php";
 
-    // signIn
+// signIn
 
-    if (isset($_POST['login-btn'])) {
+if (isset($_POST['login-btn'])) {
 
-        $username_login = $_POST['username-login'];
+    $username_login = $_POST['username-login'];
 
-        $password_login = MD5($_POST['password-login']);
+    $password_login = MD5($_POST['password-login']);
 
 
 
-        $sql = "SELECT * FROM users WHERE username='$username_login' AND password='$password_login'";
+    $sql = "SELECT * FROM users WHERE BINARY username='$username_login' AND password='$password_login'";
 
-        $rs = mysqli_query($conn, $sql);
+    $rs = mysqli_query($conn, $sql);
 
-        $fRows = mysqli_fetch_row($rs);
+    $fRows = mysqli_fetch_row($rs);
 
-        $numRows = mysqli_num_rows($rs);
+    $numRows = mysqli_num_rows($rs);
 
-        if ($numRows > 0) {
+    if ($numRows > 0) {
 
-            $_SESSION['users_id'] = $fRows[0];
+        $_SESSION['users_id'] = $fRows[0];
 
-            $_SESSION['username'] = $username_login;
+        $_SESSION['username'] = $username_login;
 
-            $_SESSION['userstype'] = $fRows[7];
+        $_SESSION['userstype'] = $fRows[7];
 
-            if ($fRows[7] == 1) {
+        if ($fRows[7] == 1) {
 
-                echo "<script>window.location='./users-merchant/index.php';</script>";
-
-            } else {
-
-                if ($fRows[7] == 2) {
-
-                    echo "<script>window.location='./users-market/index.php';</script>";
-
-                } else {
-
-                    echo "<script>window.location='./users-admin/banner.php';</script>";
-
-                }
-
-            }
-
+            echo "<script>window.location='./users-merchant/index.php';</script>";
         } else {
 
-            echo "<script>autherror();</script>";
+            if ($fRows[7] == 2) {
 
+                echo "<script>window.location='./users-market/index.php';</script>";
+            } else {
+
+                echo "<script>window.location='./users-admin/banner.php';</script>";
+            }
         }
+    } else {
 
+        echo "<script>autherror();</script>";
     }
+}
 
 
 
-    
 
-    
 
-    ?>
+
+
+?>
